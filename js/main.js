7b527159f2a18942bd3f2438ebdb8af7aa0ed082
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2025-04-15 14:39:14 +0700

    add js navbar-actions

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+const searchTrigger = document.getElementById('searchTrigger');
+const searchInput = document.querySelector('.search-input');
+
+searchTrigger.addEventListener('click', function() {
+    searchInput.classList.toggle('active');
+    
+    if (searchInput.classList.contains('active')) {
+        searchInput.focus();
+    }
+});
+
+// Закрытие при клике вне области
+document.addEventListener('click', function(e) {
+    if (!e.target.closest('.search-container')) {
+        searchInput.classList.remove('active');
+    }
+});
+
+// Закрытие при нажатии ESC
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+        searchInput.classList.remove('active');
+    }
+});
+
+// После открытия инпута проверяем адаптивность
+window.addEventListener('resize', () => {
+  const searchInput = document.querySelector('.search-input');
+  if(window.innerWidth < 768 && searchInput.classList.contains('active')) {
+    searchInput.classList.remove('active');
+  }
+});
+
+
+// Действия с контатктами
+document.querySelector('.action-contact').addEventListener('click', function() {
+  this.classList.toggle('active');
+});
