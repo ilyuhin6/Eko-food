871ae4d41cb58c1d8bc2e5c8bf15d03f2f0085c3
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2025-04-24 13:58:25 +0700

    add: image about

--- about.php
+++ about.php
@@ -9,6 +9,14 @@ include_once('./template-php/header-page.php');
 ?>
 
 
+<section class="section-page-about">
+  <div class="container">
+    <div class="page-about-wrapper">
+      <img src="./img/about/about-image.pnp" alt="" class="page-about-img">
+    </div>
+  </div>
+</section>
+
 
 
 
