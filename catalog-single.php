442f851fc9fb05b9821f080f4570e04bfa3a3e5b
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2025-05-05 12:11:55 +0700

    add: catalog-single.php

---
+++ catalog-single.php
@@ -0,0 +1,187 @@
+<?php
+$page_title = 'Мука';
+$page_header_bg = 'header-page-shop-single';
+include_once('./template-php/navbar.php');
+?>
+
+
+
+<?php
+include_once('./template-php/header-page.php');
+?>
+
+
+<style>
+  .section-product-more {
+    margin-top: 8.25rem;
+  }
+
+  .product-shop-wrapper {
+    display: flex;
+    justify-content: space-between;
+    gap: 20;
+  }
+
+  .product-shop-descr {
+    max-width: 40.625rem;
+  }
+
+  .block-link-product-shop {
+    margin-top: 3.875rem;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  .link-product-shop {
+    padding: 1.75rem 3.25rem;
+  }
+
+  .product-shop-slider {
+    max-width: 50%;
+  }
+
+  .product-card-shop {
+    background-color: #f9f8f8;
+    padding: 2.5rem 1rem 2.5rem 1rem;
+
+  }
+
+  .mySwiper3 {
+    margin-top: 1rem;
+  }
+
+  @media (max-width:1200px) {
+    .product-shop-wrapper {
+      flex-direction: column;
+      align-items: center;
+      gap: 20px;
+    }
+
+    .product-shop-slider {
+      max-width: 100%;
+    }
+
+    .product-shop-descr {
+      max-width: 80%;
+    }
+  }
+
+  @media (max-width: 448px) {
+    .link-product-shop {
+      display: inline-block;
+      margin-bottom: 10px;
+    }
+
+    .card-shop-pagination {
+      width: 40px;
+      height: 100%;
+    }
+  }
+</style>
+
+<section class="section section-product-shop" style="margin-top: 8.3125rem;">
+  <div class="container">
+    <div class="product-shop-wrapper">
+      <div class="product-shop-slider">
+
+        <!-- Swiper -->
+
+        <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff" class="swiper mySwiper2">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" />
+            </div>
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" />
+            </div>
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" />
+            </div>
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" />
+            </div>
+
+          </div>
+          <div class="swiper-button-next"></div>
+          <div class="swiper-button-prev"></div>
+        </div>
+        <div thumbsSlider="" class="swiper mySwiper3">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" width="107" height="132" class="card-shop-pagination" />
+            </div>
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" width="107" height="132" class="card-shop-pagination" />
+            </div>
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" width="107" height="132" class="card-shop-pagination" />
+            </div>
+            <div class="swiper-slide product-card-shop">
+              <img src="./img/product/image2.jpg" width="107" height="132" class="card-shop-pagination" />
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="product-shop-descr">
+        <h2 class="reviews-remark">Мука Рисовая</h2>
+        <p class="about-text">
+          Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
+        </p>
+        <p class="about-text">
+          Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.
+        </p>
+        <div class="block-link-product-shop">
+          <a href="" class="link-product-shop" style="background-color: #C01F93; color: #fff; margin-right: 20px;">Wildberries</a>
+          <a href="" class="link-product-shop" style="background-color: #2714FF; color: #fff; border-radius: 15px; padding: 1.75rem 4.9375rem;">OZON</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section section-product-more">
+  <div class="container">
+    <h2 class="section-product-remark">Похожие продукты</h2>
+
+    <div class="product-wrapper">
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+    </div>
+  </div>
+
+</section>
+
+
+
+
+<?php
+include_once('./template-php/footer.php');
+?>
--- template-php/section-product.php
+++ template-php/section-product.php
@@ -3,49 +3,65 @@
     <!-- <h2 class="section section-product-title">Категории</h2>
     <p class="section-product-remark">Наши продукты</p> -->
     <div class="product-wrapper">
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image1.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
     </div>
 
     <div class="product-wrapper">
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
-      <div class="product-card">
-        <span class="badge-product">Масла</span>
-        <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
-        <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
-      </div>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
+      <a href="../catalog-single.php" class="product-card-link">
+        <div class="product-card">
+          <span class="badge-product">Масла</span>
+          <img src="./img/product/image2.jpg" alt="масло" width="292" height="361">
+          <h3 class="card-product-title">Масло тыквенное, 250мл</h3>
+        </div>
+      </a>
     </div>
 
     <!-- <a href="#" class="link link-primary link-product-page">Смотреть всё <img src="./img/icon/Aerrow.svg" alt="смотреть все" width="19" height="19" /></a> -->
